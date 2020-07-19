23ed6dd4f361f8f666314b51abd6eaa19521062b
ajncespedes <ajncespedes@gmail.com>
2020-07-19 12:18:14 +0200

    Basic project structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Movies recommendation</h1>
+    </div>
+  );
+}
+
+export default App;
