f32da02a026bc3199eba576bbca5e30919e8218d
ajncespedes <ajncespedes@gmail.com>
2020-09-15 19:36:56 +0200

    Changed http to https

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -1,5 +1,5 @@
 export const OMDB_API_KEY = process.env.REACT_APP_OMDB_API_KEY;
 export const TASTEDIVE_API_KEY = process.env.REACT_APP_TASTEDIVE_API_KEY;
-export const OMDB_URL = 'http://www.omdbapi.com/';
+export const OMDB_URL = 'https://www.omdbapi.com/';
 export const TASTEDIVE_URL = 'https://tastedive.com/api/similar';
 export const CORS_ANYWHERE_API = 'https://cors-anywhere.herokuapp.com/';
