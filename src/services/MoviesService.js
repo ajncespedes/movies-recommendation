3bdd87ad382bd8a25174863f70c4b923e319a2c6
ajncespedes <ajncespedes@gmail.com>
2020-12-13 21:06:53 +0100

    Remove all console logs

--- src/services/MoviesService.js
+++ src/services/MoviesService.js
@@ -3,7 +3,6 @@ import { OMDB_API_KEY, OMDB_URL, TASTEDIVE_API_KEY, TASTEDIVE_URL, CORS_ANYWHERE
 export async function getMovieById({ id }) {
     const data = await fetch(`${OMDB_URL}?apikey=${OMDB_API_KEY}&i=${id}&type=movie&plot=full`);
     const movie = await data.json();
-    console.log('info', movie);
     
     return movie;
 }
@@ -12,7 +11,6 @@ export async function getMovieByName({ Name }) {
     const data = await fetch(`${OMDB_URL}?apikey=${OMDB_API_KEY}&s=${Name}&type=movie`);
     const dataJson = await data.json();
     const movie = dataJson.Search ? dataJson.Search[0] : null;
-    console.log('Movies with the name: ' + Name, dataJson.Search);
 
     return movie;
 }
@@ -21,8 +19,6 @@ export async function getSimilarMovies({ Title }) {
     const data = await fetch(`${CORS_ANYWHERE_API}${TASTEDIVE_URL}?k=${TASTEDIVE_API_KEY}&q=${Title}&type=movies&info=1&limit=8`);
     const dataJson = await data.json();
     const movies = dataJson.Similar.Results;
-
-    console.log('Movies similar to: ' + Title, movies);
 
     let newMovies = [];
     for(const movie of movies) {
